9c8ad197520636dbd9c664dc7fcf5dcfccda267b
Ilham_pplg2                                                                              . <113089275+ilhamsudrajat@users.noreply.github.com>
2024-10-16 08:59:49 +0700

    b

--- admin/index.php
+++ admin/index.php
@@ -12,7 +12,7 @@ echo "Selamat datang, " . $_SESSION['username'] . "!";
 <html>
  <head>
   <title>
-   HAMS
+   ilhjam
   </title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&amp;display=swap" rel="stylesheet"/>
